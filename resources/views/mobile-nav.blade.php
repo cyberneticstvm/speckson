05bb1d8f0a12ad449ad95b653ef00d97b042cf4e
Cybernetics <mail@cybernetics.me>
2026-02-05 15:30:21 +0530

    first commit

---
+++ resources/views/mobile-nav.blade.php
@@ -0,0 +1,49 @@
+<!-- mobile menu area start -->
+<div class="mobile-menu-main">
+    <nav class="nav-main mainmenu-nav mt--30">
+        <ul class="mainmenu metismenu" id="mobile-menu-active">
+            <li>
+                <a class="main" href="{{ route('index') }}">Glasses</a>
+            </li>
+            <li>
+                <a class="main" href="{{ route('index') }}">Contact Lenses</a>
+            </li>
+            <li>
+                <a class="main" href="{{ route('index') }}">Offers</a>
+            </li>
+            <li class="has-dropdown">
+                <a class="nav-link" href="#">Blogs</a>
+                <ul class="submenu">
+                    <li class="lim"><a class="sdm-main" href="blog.html">Coming Soon</a></li>
+                </ul>
+            </li>
+            <li><a href="contact.html" class="main">Contact</a></li>
+        </ul>
+    </nav>
+
+    <div class="rts-social-style-one pl--20 mt--100">
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-brands fa-twitter"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-brands fa-youtube"></i>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- mobile menu area end -->
